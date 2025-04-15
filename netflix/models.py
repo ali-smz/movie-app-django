c587360239dde8ca56adebbbf96adbf077d12f84
unknown <samiizadeh.ali@gmail.com>
2025-04-15 18:20:40 +0330

    movie model developed and dummy datas added

--- netflix/models.py
+++ netflix/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    GENRE_CHOICES = [
+        ('Action', 'Action'),
+        ('Comedy', 'Comedy'),
+        ('Drama', 'Drama'),
+        ('Horror', 'Horror'),
+        ('Sci-Fi', 'Sci-Fi'),
+        ('Romance', 'Romance'),
+        ('Thriller', 'Thriller'),
+        ('Documentary', 'Documentary'),
+        ('Animation', 'Animation'),
+    ]
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    release_date = models.DateField()
+    image = models.URLField()
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)
+    
+    def __str__(self):
+        return self.title
