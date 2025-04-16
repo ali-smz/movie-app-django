3c3d2bd48e2359c196bfa9fa7d0e85d16e59120a
unknown <samiizadeh.ali@gmail.com>
2025-04-17 00:46:59 +0330

    movie list api added

---
+++ netflix/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from .views import MovieList
+
+urlpatterns = [
+    path('movie-list/', MovieList.as_view() , name='movie-list'),
+]
--- netflix/views.py
+++ netflix/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Movie
+from .serializers import MovieSerializer
 
 # Create your views here.
+
+class MovieList(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data , status=200)
+    
