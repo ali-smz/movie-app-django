000272aa9c274f303531e9e623a9e593a18b6c90
unknown <samiizadeh.ali@gmail.com>
2025-04-18 01:38:50 +0330

    GetMovie view and url added

--- netflix/urls.py
+++ netflix/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import MovieList
+from .views import MovieList , GetMovie
 
 urlpatterns = [
     path('movie-list/', MovieList.as_view() , name='movie-list'),
+    path('movie/<str:slug>', GetMovie.as_view() , name='movie')
 ]
--- netflix/views.py
+++ netflix/views.py
@@ -1,7 +1,7 @@
+from netflix.models import Movie
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Movie
 from .serializers import MovieSerializer
 
 # Create your views here.
@@ -11,4 +11,10 @@ class MovieList(APIView):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data , status=200)
-    
+
+
+class GetMovie(APIView):
+    def get(self, request, slug):
+            movie = Movie.objects.get(slug=slug)
+            serializer = MovieSerializer(movie)
+            return Response(serializer.data , status=200)
